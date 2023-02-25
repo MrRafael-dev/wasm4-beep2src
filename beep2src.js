dbd172f350c06e311073aa4446d457f0c5c84d8f
Hanski <hannu.j.viitala@gmail.com>
2023-02-25 15:36:32 +0200

    Cleaned up the code.

--- beep2src.js
+++ beep2src.js
@@ -17,12 +17,12 @@ Version 0.0.0.1
 Usage:
   * node beep2src {@path} {@resname} {@incdriver} {@language} {@source channel} {@max sequences}
 
-@path: path to BeepBox's JSON file.
-@resname: Resource name (that is, the variable name you want to use in code).
-@incdriver: use "true" to include the driver.
-@language: the programming language to use. Currently supported: "rust", "go" or "cpp".
+@path: The path to BeepBox's JSON file.
+@resname: A resource name (that is, the variable name you want to use in code).
+@incdriver: Use "true" to include the driver code at the top.
+@language: The programming language to use. Currently supported: "rust", "go" or "cpp".
 @source channel: The music channel to select in the source file. Only one channel can be selected.
-@max sequences: The maximun number of squences to process.
+@max sequences: The maximum number of squences to process.
 
 Example:
 * node beep2src ./Bach.json Bach_melody true cpp 0 6 >../my-wasm-project/src/sounds.cpp
@@ -30,14 +30,11 @@ Example:
 Notes:
   * Scale must be "expert", key must be "C".
   * Only the very first instrument will be converted.
-  * It only works with one track.
-  * Each track has support for 32 notes.
   * This may or may not be updated (it's just a PoC).
-  * Only Rust is supported.
+  * Only Rust, Go and C++ are supported.
 ===========================================================
 BeepBox:
-  * Official website: https://www.beepbox.co/2_3/
-  * Only version 2.3 is supported.
+  * Official website: https://www.beepbox.co/
 ===========================================================
 `);
   return;
@@ -75,35 +72,11 @@ if(arguments[2].toString().trim().toLowerCase() === "true") {
  *   end of the music.
  */
 let BeepBox = {
-  /** Musical note indexes, sorted by pitch. */
-  //notes: [
-  //  24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 
-  //  36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 
-  //	48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,  
-  //	60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
-  //   72, 72, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
-   //84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
-  // ],
-
+	
+  /** Note value mapping */
   firstTone : 24,  // The first tone.
   lastTone : 83,  // The last tone.
 	
-  /** Musical note names. */
-  //names: [
-  //    "C0", "D+0",  "D0", "E+0",  "E0",  "F0", "F#0",  "G0", "A+0",  "A0", "B+0",  "B0",  
-  //	  "C1", "D+1",  "D1", "E+1",  "E1",  "F1", "F#1",  "G1", "A+1",  "A1", "B+1",  "B1",  
-  //	  "C2", "D+2",  "D2", "E+2",  "E2",  "F2", "F#2",  "G2", "A+2",  "A2", "B+2",  "B2", 
-  //	  "C3", "D+3",  "D3", "E+3",  "E3",  "F3", "F#3",  "G3", "A+3",  "A3", "B+3",  "B3",
-  //	  "C4", "D+4",  "D4", "E+4",  "E4",  "F4", "F#4",  "G4", "A+4",  "A4", "B+4",  "B4",
-  //],
-  /** WASM-4 equivalent of musical tones, using the same musical notes. */
-  //tones: [
-  //    10,   20,   30,   40,   50,   60,   70,   80,   90,  100,  110,  120,  
-  //   130,  140,  150,  160,  170,  180,  190,  200,  210,  220,  230,  250,  
-  //	 260,  280,  290,  310,  330,  350,  370,  390,  410,  440,  460,  490,  
-  //	 520,  550,  600,  620,  660,  700,  750,  780,  840,  880,  940,  980,
-  //  1000, 1010, 1020, 1030, 1040, 1050, 1060, 1070, 1080, 1090, 2000, 2020, 
-  //],
   /** Waves (instruments) available for use. */
   waves: [
     "triangle",
@@ -136,23 +109,13 @@ let Music = {
  * @return {Object}
  */
 Music.createNote = function(note, tickOffset) {
-  // Get note index...
-  //let noteIndex = BeepBox.notes.indexOf(note.pitches[0]);
-
+  // Get the converted tone value for the note.
   let toneVal = 0;
 
-  if(note.pitches[0] < BeepBox.firstTone || note.pitches[0]> BeepBox.lastTone) {
-	console.log('!!HV: createNote(). invalid note. pitch: %d', note.pitches[0]);
+  if(note.pitches[0] < BeepBox.firstTone || note.pitches[0]> BeepBox.lastTone)
     toneVal = 0xFF;
-  } else {
+  else
   	toneVal = note.pitches[0] - BeepBox.firstTone;
-  }
-
-  // Check if note index is valid...
-  //if(noteIndex <= 0 || isNaN(noteIndex)) {
-  //	console.log('!!HV: createNote(). invalid note. pitch: %d', note.pitches[0]);
-  //    noteIndex = 0xFF;
-  //}
 
   // Description object.
   let noteObject = {
@@ -200,9 +163,6 @@ Music.print = function(lang) {
   // Iterate through each note and concatenate text...
   for(let i = 0; i <= Music.finalTick; i += 1) {
     let note = Music.notes[i] || Music.createNoteEmpty();
-	  
-	  //if(!Music.notes[i]) 
-	  //	  console.log('!!HV: Music.print(). NO NOTE! i=%d', i);  
 	  
 	if(lang=="rust")
     	text += `(0x${note.tone.toString(16).padStart(2,0)},0x${note.sustain.toString(16).padStart(2,0)},0x00),`;
@@ -258,14 +218,13 @@ for(let i=0; i<numberOfSequencesToProcess && channel.sequence[i]; i+=1) {
 
 	  // Save music...
 	  Music.notes[noteObject.start] = noteObject;
-	  //console.log('!!HV: Stored Music.notes[%d]', noteObject.start);
 
 	  lastTickEnd = noteObject.end;
 	}
 
 	tickOffset = lastTickEnd;
 
-}  // end patterns
+}  // end sequence
 
 Music.finalTick = tickOffset;
 
