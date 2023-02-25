4f0fc01ae454e5a3e3a9ba4f824d75353f31bba0
Hanski <hannu.j.viitala@gmail.com>
2023-02-25 14:20:49 +0200

    Added support for C++ output. Handles sequences in reading from json. The score is still outputted as single long pattern of notes. The volume added to the Track struct so now the note volume can be given in the step() call. Now supports 5 octaves. Added new "instrument", channel 1, when playing the score.  Previouisly only channel 0 and 1 were supported.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # wasm4-beep2src
-Experimental music converter for WASM-4.
+Experimental music converter for WASM-4. The script generates WASM-4 code from the selected channel in the BeepBox json file. Currently Rust, Go, and C++ source file generation is supported.
+
+# How to use (C++)
+
+There are only 4 channels in WASM-4 and some of thenm are possibly used for sound effects. The beep2src script can be used to generate the scores from the selected channel in the BeepBox json file like below.
+
+1) You can generate the C++ code of the melody channel (templated added, channel 0, create new file) like this:
+  node beep2src ./Bach.json Bach_melody true cpp 0 >../my-wasm-project/src/sounds.cpp
+
+2) Then you can also add one (or more) another channel (no template added, channel 3, append to the file) if you want:
+  node beep2src ./Bach.json Bach_bass false cpp 3 >> ../my-wasm-project/src/sounds.cpp
+  
+To play the two channels you can first add these definitions at the top of your main.cpp file:
+  
+  // These are implemented in the generated sounds.cpp file.
+  void step_BACH_MELODY();
+  void step_BACH_BASS();
+  
+And then call them in your update() function. You can call those on each iteration of the update() method if that do not sound too fast.
+
+  // Run the sound engine step with my scores.
+  step_BACH_MELODY();
+  step_BACH_BASS();
+  
+
+
+
--- beep2src.js
+++ beep2src.js
@@ -5,7 +5,7 @@ let fs = require("fs");
 var arguments = process.argv.slice(2);
 
 /** Expected arguments to be received. */
-var expectedArguments = 3;
+var expectedArguments = 6;
 
 // Display usage when not enough arguments are passed...
 if(arguments.length < expectedArguments) {
@@ -15,11 +15,17 @@ BeatBox to WASM-4 Importer
 Version 0.0.0.1
 ===========================================================
 Usage:
-  * node beep2src {@path} {@resname} {@incdriver}
+  * node beep2src {@path} {@resname} {@incdriver} {@language} {@source channel} {@max sequences}
 
 @path: path to BeepBox's JSON file.
-@resname: Resource name (that is, the variable).
+@resname: Resource name (that is, the variable name you want to use in code).
 @incdriver: use "true" to include the driver.
+@language: the programming language to use. Currently supported: "rust", "go" or "cpp".
+@source channel: The music channel to select in the source file. Only one channel can be selected.
+@max sequences: The maximun number of squences to process.
+
+Example:
+* node beep2src ./Bach.json Bach_melody true cpp 0 6 >../my-wasm-project/src/sounds.cpp
 ===========================================================
 Notes:
   * Scale must be "expert", key must be "C".
@@ -45,7 +51,9 @@ let data = JSON.parse(file);
 
 /** Templates in use. */
 let templates = {
-  driver: fs.readFileSync("./templates/driver.rs", "utf8")
+  	"rust": 	fs.readFileSync("./templates/driver.rs", "utf8"),
+  	"go":  		fs.readFileSync("./templates/driver.go", "utf8"),
+  	"cpp": 		fs.readFileSync("./templates/driver.cpp", "utf8")
 };
 
 // Final result.
@@ -53,7 +61,8 @@ let result = "";
 
 // Include driver (optional)...
 if(arguments[2].toString().trim().toLowerCase() === "true") {
-  result += templates.driver;
+	let keyLang = arguments[3].toString().trim().toLowerCase();
+  	result += templates[keyLang];
 }
 
 /**
@@ -67,29 +76,34 @@ if(arguments[2].toString().trim().toLowerCase() === "true") {
  */
 let BeepBox = {
   /** Musical note indexes, sorted by pitch. */
-  notes: [
-    36, 37, 38, 39, 40, 41, 42, 43, 44,
-    45, 46, 47, 48, 49, 50, 51, 52, 53,
-    54, 55, 56, 57, 58, 59, 60, 61, 62,
-    63, 64, 65, 66, 67, 68, 69, 70, 71,
-    72
-  ],
+  //notes: [
+  //  24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 
+  //  36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 
+  //	48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,  
+  //	60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,
+  //   72, 72, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,
+   //84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+  // ],
+
+  firstTone : 24,  // The first tone.
+  lastTone : 83,  // The last tone.
+	
   /** Musical note names. */
-  names: [
-     "C0", "D+0",  "D0", "E+0",  "E0",  "F0", "F#0",  "G0", "A+0",
-     "A0", "B+0",  "B0",  "C1", "D+1",  "D1", "E+1",  "E1",  "F1",
-    "F#1",  "G1", "A+1",  "A1", "B+1",  "B1",  "C2", "D+2",  "D2",
-    "E+2",  "E2",  "F2", "F#2",  "G2", "A+2",  "A2", "B+2",  "B2",
-     "C3"
-  ],
+  //names: [
+  //    "C0", "D+0",  "D0", "E+0",  "E0",  "F0", "F#0",  "G0", "A+0",  "A0", "B+0",  "B0",  
+  //	  "C1", "D+1",  "D1", "E+1",  "E1",  "F1", "F#1",  "G1", "A+1",  "A1", "B+1",  "B1",  
+  //	  "C2", "D+2",  "D2", "E+2",  "E2",  "F2", "F#2",  "G2", "A+2",  "A2", "B+2",  "B2", 
+  //	  "C3", "D+3",  "D3", "E+3",  "E3",  "F3", "F#3",  "G3", "A+3",  "A3", "B+3",  "B3",
+  //	  "C4", "D+4",  "D4", "E+4",  "E4",  "F4", "F#4",  "G4", "A+4",  "A4", "B+4",  "B4",
+  //],
   /** WASM-4 equivalent of musical tones, using the same musical notes. */
-  tones: [
-     130,  140,  150,  160,  170,  180,  190,  200,  210,
-     220,  230,  250,  260,  280,  290,  310,  330,  350,
-     370,  390,  410,  440,  460,  490,  520,  550,  600,
-     620,  660,  700,  750,  780,  840,  880,  940,  980,
-    1000
-  ],
+  //tones: [
+  //    10,   20,   30,   40,   50,   60,   70,   80,   90,  100,  110,  120,  
+  //   130,  140,  150,  160,  170,  180,  190,  200,  210,  220,  230,  250,  
+  //	 260,  280,  290,  310,  330,  350,  370,  390,  410,  440,  460,  490,  
+  //	 520,  550,  600,  620,  660,  700,  750,  780,  840,  880,  940,  980,
+  //  1000, 1010, 1020, 1030, 1040, 1050, 1060, 1070, 1080, 1090, 2000, 2020, 
+  //],
   /** Waves (instruments) available for use. */
   waves: [
     "triangle",
@@ -109,7 +123,9 @@ let Music = {
   /** Instrument in use. */
   instrument: 0,
   /** Track notes. */
-  notes: []
+  notes: [],
+  /* The last tick processed */
+  finalTick: 0,
 };
 
 /**
@@ -119,21 +135,31 @@ let Music = {
  *
  * @return {Object}
  */
-Music.createNote = function(note) {
+Music.createNote = function(note, tickOffset) {
   // Get note index...
-  let noteIndex = BeepBox.notes.indexOf(note.pitches[0]);
+  //let noteIndex = BeepBox.notes.indexOf(note.pitches[0]);
+
+  let toneVal = 0;
+
+  if(note.pitches[0] < BeepBox.firstTone || note.pitches[0]> BeepBox.lastTone) {
+	console.log('!!HV: createNote(). invalid note. pitch: %d', note.pitches[0]);
+    toneVal = 0xFF;
+  } else {
+  	toneVal = note.pitches[0] - BeepBox.firstTone;
+  }
 
   // Check if note index is valid...
-  if(noteIndex <= 0 || isNaN(noteIndex)) {
-    noteIndex = 0xFF;
-  }
+  //if(noteIndex <= 0 || isNaN(noteIndex)) {
+  //	console.log('!!HV: createNote(). invalid note. pitch: %d', note.pitches[0]);
+  //    noteIndex = 0xFF;
+  //}
 
   // Description object.
   let noteObject = {
     pitch   : note.pitches[0],
-    start   : note.points[0].tick,
-    end     : note.points[1].tick,
-    tone    : noteIndex,
+    start   : note.points[0].tick + tickOffset,
+    end     : note.points[1].tick + tickOffset,
+    tone    : toneVal,
     sustain : 5,
     duration: (note.points[1].tick - note.points[0].tick)
   };
@@ -167,21 +193,28 @@ Music.createNoteEmpty = function() {
  *
  * @return {string}
  */
-Music.print = function() {
+Music.print = function(lang) {
   // Text to return.
   let text = "";
 
   // Iterate through each note and concatenate text...
-  for(let i = 0; i < 32; i += 1) {
+  for(let i = 0; i <= Music.finalTick; i += 1) {
     let note = Music.notes[i] || Music.createNoteEmpty();
-    text += `(0x${note.tone.toString(16).padStart(2,0)},0x${note.sustain.toString(16).padStart(2,0)},0x00),`;
-
-    // Remove comma from last element...
-    if(i > 30) {
-      text = text.substring(0, text.length - 1);
-    }
+	  
+	  //if(!Music.notes[i]) 
+	  //	  console.log('!!HV: Music.print(). NO NOTE! i=%d', i);  
+	  
+	if(lang=="rust")
+    	text += `(0x${note.tone.toString(16).padStart(2,0)},0x${note.sustain.toString(16).padStart(2,0)},0x00),`;
+	else if(lang=="go")
+    	text += `(0x${note.tone.toString(16).padStart(2,0)},0x${note.sustain.toString(16).padStart(2,0)},0x00),`;
+	else if(lang=="cpp")
+    	text += `0x${note.tone.toString(16).padStart(2,0)},0x${note.sustain.toString(16).padStart(2,0)},0x00 ,`;
   }
 
+  // Remove comma from last element...
+  text = text.substring(0, text.length - 1);
+	
   return text;
 };
 
@@ -195,27 +228,46 @@ for(let i = 0; i < 32; i += 1) {
 let ticks = data.ticksPerBeat;
 
 /** Main channel (only the first track is supported). */
-let channel = data.channels[0];
+let selectedChannel = arguments[4];
+let numberOfSequencesToProcess = arguments[5];
+let channel = data.channels[selectedChannel];
 
 /** Wave name in use. */
 let instrument = channel.instruments[0];
 
-/** Track pattern (only the first is supported). */
-let pattern = channel.patterns[0];
+/** Patterns **/
+let tickOffset = 0;
+for(let i=0; i<numberOfSequencesToProcess && channel.sequence[i]; i+=1) {
 
-/** Soundtrack notes. */
-let notes = pattern.notes;
+	/** Get the sequence **/
+	let sequence = channel.sequence[i];
+	
+	/** Track pattern of notes */
+	let pattern = channel.patterns[sequence];
 
-// Iterate through the track...
-for(let noteIndex in notes) {
-  let note = notes[noteIndex];
+	/** Soundtrack notes. */
+	let notes = pattern.notes;
 
-  // Description object.
-  let noteObject = Music.createNote(note);
+	// Iterate through the track...
+	lastTickEnd = 0;
+	for(let noteIndex in notes) {
+	  let note = notes[noteIndex];
 
-  // Save music...
-  Music.notes[noteObject.start] = noteObject;
-}
+	  // Description object.
+	  let noteObject = Music.createNote(note, tickOffset);
+
+	  // Save music...
+	  Music.notes[noteObject.start] = noteObject;
+	  //console.log('!!HV: Stored Music.notes[%d]', noteObject.start);
+
+	  lastTickEnd = noteObject.end;
+	}
+
+	tickOffset = lastTickEnd;
+
+}  // end patterns
+
+Music.finalTick = tickOffset;
 
 // Get instrument index...
 Music.instrument = BeepBox.waves.indexOf(instrument.wave);
@@ -226,16 +278,66 @@ if(Music.instrument < 0) {
 }
 
 // Print track...
-result += `/// Soundtrack: *${Music.name}*
+let lang = arguments[3].toString().trim().toLowerCase();
+if(lang=="rust") {
+	
+// *** RUST ***
+result += `
+/// Soundtrack: *${Music.name}*
 static mut ${Music.name}: Track = Track {
     next      : 0,
     wait      : 0,
     ticks     : ${ticks},
     instrument: ${Music.instrument},
+    volume    : 100,
     flags     : (0,0),
-    tones: [ ${Music.print()} ],
+    tones: [ ${Music.print(lang)} ],
 };
 `;
+
+} else if(lang=="go") {
+	
+// *** GO ***
+result += `
+/// Soundtrack: *${Music.name}*
+static mut ${Music.name}: Track = Track {
+    next      : 0,
+    wait      : 0,
+    ticks     : ${ticks},
+    instrument: ${Music.instrument},
+	volume    : 100,
+    flags     : (0,0),
+    tones: [ ${Music.print(lang)} ],
+};
+`;
+	
+// *** CPP ***
+} else if(lang=="cpp") {
+	
+result += `
+/// Soundtrack: *${Music.name}*
+const uint8_t ${Music.name}_tones[] =  { 
+ ${Music.print(lang)} 
+};
+
+Track ${Music.name} = {
+    /*next*/ 0,
+    /*wait*/ 0,
+    /*ticks*/ ${ticks},
+    /*instrument*/ ${Music.instrument},
+	/*volume*/ 100,
+    /*flags*/ {0,0},
+    /*tones*/ (uint8_t*)${Music.name}_tones,
+    /*tone count*/ sizeof(${Music.name}_tones)/3
+};
+
+void step_${Music.name}()
+{
+	step( ${Music.name} );
+}
+`;
+	
+}
 
 // Result.
 console.log(result);
